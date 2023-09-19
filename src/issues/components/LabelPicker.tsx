8513353137ae2eefc8d8da9908ddefa7e7853e8f
Tomás Hernández <thernandez@ptn.gob.ar>
2023-09-19 10:27:27 -0300

    icons and react query

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,13 +1,27 @@
+import { LoadingIcon } from "../../shared/components/LoadingIcon";
+import { useLabels } from "../hooks/useLabels"
 
 export const LabelPicker = () => {
+
+  const labelsQuery = useLabels();
+
+  if(labelsQuery.isLoading)
+    return ( <LoadingIcon/> )
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
+      {
+        labelsQuery.data?.map((label) => (
+          <span
+              key={label.id} 
+              className="badge rounded-pill m-1 label-picker"
+              style={{ border: `1px solid #${label.color}`, color: `#${label.color}` }}
+          >
+              {label.name}
+          </span>
+        ))
+      }
+        
         
     </div>
   )
---
+++ src/issues/hooks/useLabels.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query"
+import { githubApi } from "../../api/githubApi";
+import { Label } from "../interfaces/label";
+import { sleep } from "../../helpers/sleep";
+
+const getLabels = async():Promise<Label[]> => {
+  await sleep(2);
+  const { data } = await githubApi.get<Label[]>('/labels');
+  return data;
+}
+
+export const useLabels = () => {
+
+    const labelsQuery = useQuery(
+        ['labels'],
+        getLabels,
+        {
+            staleTime: 1000 * 60 * 60
+        }
+      ) 
+
+      return labelsQuery
+      
+}
